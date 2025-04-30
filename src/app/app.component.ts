15553e78bb95c3fbdbd6246948ab8bed979d3681
ranjandevwork <ranjandswn95@gmail.com>
2025-05-01 00:41:41 +0530

    adding bootstarp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngrx-counter';
+  title = 'ngrx counter using standalone components';
 }
